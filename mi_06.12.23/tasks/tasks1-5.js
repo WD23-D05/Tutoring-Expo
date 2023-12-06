87285633906e02c69ef909d0eb9ef8b8be8f671b
Expolicious <b.exposito@icloud.com>
2023-12-06 19:48:10 +0100

    Aufgaben Mi 06.12.23

---
+++ mi_06.12.23/tasks/tasks1-5.js
@@ -0,0 +1,16 @@
+/*
+Aufgabe 1: Grundlegende Nested Components
+Erstelle eine React-Anwendung, die aus zwei Komponenten besteht: App und Header. Die App-Komponente sollte die Header-Komponente als nested Component enthalten. Die Header-Komponente soll eine einfache Überschrift (Header) rendern.
+
+Aufgabe 2: Props in Nested Components
+Erweitere die vorherige Anwendung, indem du der Header-Komponente eine prop namens title übergibst. Verwende dann diese prop, um den Titel dynamisch in der Header-Komponente anzuzeigen.
+
+Aufgabe 3: Nested Components mit Zustand
+Erstelle eine neue Komponente namens Counter, die einen einfachen Zähler darstellt. Die App-Komponente sollte den Counter als nested Component enthalten. Der Zähler sollte einen Zustand (count) verwenden und die Möglichkeit bieten, den Zähler zu erhöhen.
+
+Aufgabe 4: Styling von Nested Components
+Füge Styles zu den vorherigen Komponenten hinzu, um das Aussehen der Anwendung zu verbessern. Verwende CSS oder eine Styling-Bibliothek deiner Wahl.
+
+Aufgabe 5: Verwendung von Props.children
+Erstelle eine Wrapper-Komponente namens Wrapper, die andere Komponenten umschließt. Die Wrapper-Komponente sollte den Inhalt der umschlossenen Komponenten anzeigen. Verwende die props.children-API, um dies zu erreichen.
+*/
